d7bbc53c832034f33444b66517d60f18390c4526
Pol Gubau Amores <gubaupol@gmail.com>
2025-03-01 18:15:20 +0100

    chore: 🗑️ remove unused image asset aina.png

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
 
 <template>
   <div>
-    <img src="@/assets/aina.png" alt="a"/>
+    <!-- <img src="@/assets/img1.png" alt="a"/> -->
     <h1>Home Page</h1>
     <p>Welcome to the home page.</p>
     <Alert>Hello</Alert>
